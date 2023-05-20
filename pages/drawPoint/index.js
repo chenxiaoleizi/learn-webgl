e4123fe27d782a33a1aa0a6e4f7a27dc57da279b
chenlei <791563973@qq.com>
2023-05-21 07:45:09 +0800

    feat: replace tweakpane with lil-gui

--- pages/drawPoint/index.js
+++ pages/drawPoint/index.js
@@ -1,9 +1,10 @@
-import {Pane} from "tweakpane"
+import GUI from 'lil-gui';
 import {getGl, createShader, createProgram} from "@/core/index.js"
 
-const params = {
-  position: {x: 0, y: 0},
-  color: {r: 1, g: 0, b: 0}
+const settings = {
+  color: {r: 1, g: 0, b: 0},
+  x: 0,
+  y: 0
 }
 
 const v = `
@@ -30,33 +31,30 @@ const fShader = createShader(gl, f, gl.FRAGMENT_SHADER)
 const program = createProgram(gl, vShader, fShader)
 gl.useProgram(program)
 
-// Position
+// Position and color location
 const positionLocation = gl.getAttribLocation(program, "a_position")
-gl.vertexAttrib3f(positionLocation, params.position.x, params.position.y, 0)
-
 const colorLocation = gl.getUniformLocation(program, "u_color")
-gl.uniform4f(colorLocation, params.color.r, params.color.g, params.color.b, 1)
 
-gl.clearColor(1, 1, 1, 1)
-gl.drawArrays(gl.POINTS, 0, 1)
+function draw() {
+  // Set position and color
+  gl.vertexAttrib3f(positionLocation, settings.x, settings.y, 0)
+  gl.uniform4f(colorLocation, settings.color.r, settings.color.g, settings.color.b, 1)
+
+  gl.clearColor(1, 1, 1, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.drawArrays(gl.POINTS, 0, 1)
+}
+draw()
 
 // UI
-const pane = new Pane()
-pane.addInput(params, "position", {
-  picker: 'inline',
-  expanded: true,
-  x: {mix: -1, max: 1},
-  y: {mix: -1, max: 1}
-})
-.on("change", () => {
-  gl.vertexAttrib3f(positionLocation, params.position.x, params.position.y, 0)
-  gl.clearColor(1, 1, 1, 1)
-  gl.drawArrays(gl.POINTS, 0, 1)
-})
+const gui = new GUI()
 
-pane.addInput(params, "color", {color: {type: 'float'}})
-.on("change", () => {
-  gl.uniform4f(colorLocation, params.color.r, params.color.g, params.color.b, 1)
-  gl.clearColor(1, 1, 1, 1)
-  gl.drawArrays(gl.POINTS, 0, 1)
+gui.addColor(settings, "color")
+
+const positionFolder = gui.addFolder("Position")
+positionFolder.add(settings, "x").min(-1).max(1)
+positionFolder.add(settings, "y").min(-1).max(1)
+
+gui.onChange(() => {
+  draw()
 })
--- pages/drawPoints/index.js
+++ pages/drawPoints/index.js
@@ -1,8 +1,8 @@
-import {Pane} from "tweakpane"
+import GUI from "lil-gui"
 import {getGl, createShader, createProgram, setPosition} from "@/core/index.js"
 
-const params = {
-  pointCount: 10,
+const settings = {
+  count: 10,
   color: {r: 1, g: 0, b: 0}
 }
 
@@ -30,16 +30,14 @@ const fShader = createShader(gl, f, gl.FRAGMENT_SHADER)
 const program = createProgram(gl, vShader, fShader)
 gl.useProgram(program)
 
-// Position
+// Position and color location
 const positionLocation = gl.getAttribLocation(program, "a_position")
-
 const colorLocation = gl.getUniformLocation(program, "u_color")
-gl.uniform4f(colorLocation, params.color.r, params.color.g, params.color.b, 1)
 
 function createRandomPoints() {
   const vertices = []
 
-  for (let i = 0; i < params.pointCount; i++) {
+  for (let i = 0; i < settings.count; i++) {
     const x = Math.random() * 2 - 1
     const y = Math.random() * 2 - 1
     
@@ -49,30 +47,22 @@ function createRandomPoints() {
 }
 
 function draw() {
+  // Set position and color
+  createRandomPoints()
+  gl.uniform4f(colorLocation, settings.color.r, settings.color.g, settings.color.b, 1)
+
   gl.clearColor(1, 1, 1, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawArrays(gl.POINTS, 0, params.pointCount)
+  gl.drawArrays(gl.POINTS, 0, settings.count)
 }
-draw()
+// draw()
 
 // UI
-const pane = new Pane()
-pane.addInput(params, "pointCount", {
-  picker: 'inline',
-  expanded: true
-})
-.on("change", () => {
-  draw()
-})
+const gui = new GUI()
 
-pane.addInput(params, "color", {color: {type: 'float'}})
-.on("change", () => {
-  gl.uniform4f(colorLocation, params.color.r, params.color.g, params.color.b, 1)
-  draw()
-})
+gui.add(settings, "count").min(1).max(100)
+gui.addColor(settings, "color")
 
-pane.addButton({title: "Create points", label: "Create"})
-.on("click", () => {
-  createRandomPoints()
+gui.onChange(() => {
   draw()
 })
