adcbaa6f43e3bdebf634771db97606e0f06d2e64
chenlei <791563973@qq.com>
2023-05-23 20:59:06 +0800

    feat: fix wrong uv

--- pages/texture/index.js
+++ pages/texture/index.js
@@ -40,7 +40,7 @@ const verticesUvs = new Float32Array([
   0.5, -0.5, 0, 1, 0,
   0.5, 0.5, 0, 1, 1,
   0.5, 0.5, 0, 1, 1,
-  -0.5, 0.5, 0, 1, 0,
+  -0.5, 0.5, 0, 0, 1,
   -0.5, -0.5, 0, 0, 0
 ])
 const BYTES_PER_ELEMENT = verticesUvs.BYTES_PER_ELEMENT
